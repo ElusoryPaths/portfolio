b0b48c4561527b9dac53968487423c17e1dc5b84
Jacky Shen <83739233+ElusoryPaths@users.noreply.github.com>
2023-02-09 02:42:53 -0500

    angular build files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "home", pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
